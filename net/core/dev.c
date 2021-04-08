32f94efe66da5297d46eba8b8d4c1821a86238cc
Matteo Croce <mcroce@microsoft.com>
2021-04-08 02:57:56 +0200

    net: use skb_for_each_frag() in illegal_highdma()
    Coccinelle failed with the following error:
    
     EXN: Failure("no position information") in net/core/dev.c
    
    Apply it by hand as it's trivial.
    
    Signed-off-by: Matteo Croce <mcroce@microsoft.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3505,7 +3505,7 @@ static int illegal_highdma(struct net_device *dev, struct sk_buff *skb)
 	int i;
 
 	if (!(dev->features & NETIF_F_HIGHDMA)) {
-		for (i = 0; i < skb_shinfo(skb)->nr_frags; i++) {
+		skb_for_each_frag(skb, i) {
 			skb_frag_t *frag = &skb_shinfo(skb)->frags[i];
 
 			if (PageHighMem(skb_frag_page(frag)))
