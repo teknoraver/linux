0d401efb7be1a0d75dffeadad72848db85854759
Matteo Croce <teknoraver@meta.com>
2024-06-15 15:10:15 +0200

    ipv6: nfp

--- drivers/net/ethernet/netronome/nfp/flower/action.c
+++ drivers/net/ethernet/netronome/nfp/flower/action.c
@@ -689,7 +689,7 @@ nfp_fl_set_ip6_hop_limit_flow_label(u32 off, __be32 exact, __be32 mask,
 		ip_hl_fl->ipv6_hop_limit |= fl_hl->hop_limit &
 					    fl_hl_mask->hop_limit;
 		break;
-	case round_down(offsetof(struct ipv6hdr, flow_lbl), 4):
+	case 0:
 		if (mask & ~IPV6_FLOWINFO_MASK ||
 		    exact & ~IPV6_FLOWINFO_MASK) {
 			NL_SET_ERR_MSG_MOD(extack, "unsupported offload: invalid pedit IPv6 flow info action");
