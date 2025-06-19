supercatking/riscv-linux

drivers: kvm: vcpu: Disabled writing HENVCFG reg
HENVCFG register is not present in EIC7700 SOC. So disabled writing
this register

Signed-off-by: Pritesh Patel <pritesh.patel@einfochips.com>

arch/riscv/kvm/vcpu.c
@@ -487,7 +487,10 @@ static void kvm_riscv_vcpu_update_config(const unsigned long *isa)
 	if (riscv_isa_extension_available(isa, ZICBOZ))
 		henvcfg |= ENVCFG_CBZE;
 
+#ifndef CONFIG_SOC_SIFIVE_EIC7700
 	csr_write(CSR_HENVCFG, henvcfg);
+#endif
+
 #ifdef CONFIG_32BIT
 	csr_write(CSR_HENVCFGH, henvcfg >> 32);
 #endif

teknoraver/linux

Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

arch/riscv/kvm/vcpu.c
@@ -509,7 +509,10 @@ static void kvm_riscv_vcpu_update_config(const unsigned long *isa)
 	if (riscv_isa_extension_available(isa, ZICBOZ))
 		henvcfg |= ENVCFG_CBZE;
 
+#ifndef CONFIG_ARCH_ESWIN_EIC770X_SOC_FAMILY
 	csr_write(CSR_HENVCFG, henvcfg);
+#endif
+
 #ifdef CONFIG_32BIT
 	csr_write(CSR_HENVCFGH, henvcfg >> 32);
 #endif