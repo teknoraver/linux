ed3e2e67cd61bb10099a3da8e2b6d2f31cc8756c
Matteo Croce <teknoraver@meta.com>
2026-01-26 11:43:15 +0100

    lib/test_string.c: add benchmarks
    Add optional benchmarks for the mem{cpy,move,set} functions.
    
    Signed-off-by: Matteo Croce <teknoraver@meta.com>

--- lib/test_string.c
+++ lib/test_string.c
@@ -207,6 +207,119 @@ static __init int strspn_selftest(void)
 	return 0;
 }
 
+#ifdef CONFIG_STRING_SELFTEST_BENCHMARK
+
+#define COPIES_NUM 100
+
+static __init int memcpy_selftest_align_size(int size, bool unalign)
+{
+	ktime_t start, end;
+	s64 elapsed_ns, total_ns = 0;
+	char *buf1;
+	char *buf2;
+	int ret = 0;
+
+	buf1 = kzalloc(size, GFP_KERNEL);
+	if (!buf1)
+		return -ENOMEM;
+
+	buf2 = kzalloc(size, GFP_KERNEL);
+	if (!buf2) {
+		ret = -ENOMEM;
+		goto out_free;
+	}
+
+	for (int i = 0; i < COPIES_NUM; i++) {
+		local_irq_disable();
+		start = ktime_get();
+		memcpy(buf1 + unalign, buf2, size - unalign);
+		end = ktime_get();
+		local_irq_enable();
+		elapsed_ns = ktime_to_ns(ktime_sub(end, start));
+		total_ns += elapsed_ns;
+	}
+
+	printk(KERN_INFO "memcpy: %saligned copy of len %d: %lld MB/s\n", 
+	       unalign ? "un" : "", size,
+	       (COPIES_NUM * size * 1000000000ULL / total_ns) / (1024 * 1024));
+
+	kfree(buf2);
+
+out_free:
+	kfree(buf1);
+
+	return ret;
+}
+
+static __init int memcpy_selftest_align(int size)
+{
+	memcpy_selftest_align_size(size, false);
+	memcpy_selftest_align_size(size, true);
+
+	return 0;
+}
+
+static __init int memcpy_selftest(void)
+{
+	memcpy_selftest_align(2);
+	memcpy_selftest_align(64);
+	memcpy_selftest_align(256);
+	memcpy_selftest_align(PAGE_SIZE << MAX_ORDER);
+
+	return 0;
+}
+
+static __init int memmove_selftest_align_size(int size, bool unalign)
+{
+	ktime_t start, end;
+	s64 elapsed_ns, total_ns = 0;
+	char *buf;
+	int ret = 0;
+	const int shift = size / 10;
+
+	buf = kzalloc(size, GFP_KERNEL);
+	if (!buf)
+		return -ENOMEM;
+
+	for (int i = 0; i < COPIES_NUM; i++) {
+		local_irq_disable();
+		start = ktime_get();
+		memmove(buf + shift + unalign, buf, size - shift - unalign);
+		end = ktime_get();
+		local_irq_enable();
+		elapsed_ns = ktime_to_ns(ktime_sub(end, start));
+		total_ns += elapsed_ns;
+		cond_resched();
+	}
+
+	printk(KERN_INFO "memmove: %saligned move of len %d: %lld MB/s\n",
+	       unalign ? "un" : "", size,
+	       (COPIES_NUM * (size - shift) * 1000000000ULL / total_ns) / (1024 * 1024));
+
+	kfree(buf);
+
+	return ret;
+}
+
+static __init int memmove_selftest_align(int size)
+{
+	memmove_selftest_align_size(size, false);
+	memmove_selftest_align_size(size, true);
+
+	return 0;
+}
+
+static __init int memmove_selftest(void)
+{
+	memmove_selftest_align(2);
+	memmove_selftest_align(64);
+	memmove_selftest_align(256);
+	memmove_selftest_align(PAGE_SIZE << MAX_ORDER);
+
+	return 0;
+}
+#endif
+
 static __exit void string_selftest_remove(void)
 {
 }
@@ -244,6 +357,18 @@ static __init int string_selftest_init(void)
 	subtest = strspn_selftest();
 	if (subtest)
 		goto fail;
+
+#ifdef CONFIG_STRING_SELFTEST_BENCHMARK
+	test = 7;
+	subtest = memcpy_selftest();
+	if (subtest)
+		goto fail;
+
+	test = 8;
+	subtest = memmove_selftest();
+	if (subtest)
+		goto fail;
+#endif
 
 	pr_info("String selftests succeeded\n");
 	return 0;
